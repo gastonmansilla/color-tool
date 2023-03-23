dcf08fc68836607c7c2eb711dedf03d1a63870f7
Gaston Mansilla <gastonmansilla@MBP-de-Gaston.fibertel.com.ar>
2023-03-23 18:51:27 -0300

    (feat): copy image
    render hidden canvas with circles in order to get the image data and copy it to the clipboard

--- src/components/ColorBox.tsx
+++ src/components/ColorBox.tsx
@@ -3,6 +3,7 @@ import { CgCloseO } from "react-icons/cg";
 import { ImHeart } from "react-icons/im";
 import { IoCopy as FaCopy } from "react-icons/io5";
 import { usePalletteColors } from "../stores/palletteColors";
+import { useEffect, useRef } from "react";
 export type ColorBoxProps = {
   actions?: ("add-to-pallette" | "copy-to-clipboard")[];
   colors: string[];
@@ -17,6 +18,7 @@ export const ColorBox = ({
   onCopy,
 }: ColorBoxProps) => {
   const [, setPallette] = usePalletteColors();
+  const canvasRef = useRef<HTMLCanvasElement>(null);
 
   const onFavClick = (color: string) => {
     setPallette({ add: color });
@@ -27,43 +29,112 @@ export const ColorBox = ({
     onCopy(color);
   };
 
+  useEffect(() => {
+    const ctx = canvasRef.current?.getContext("2d");
+    if (!ctx) {
+      return;
+    }
+
+    const radius = 50;
+    const canvasWidth =
+      colors.length == 1 ? radius * 2 : colors.length * radius * 1.2 + radius;
+
+    canvasRef.current!.width = canvasWidth;
+    canvasRef.current!.height = 100;
+
+    for (let index = 0; index < colors.length; index++) {
+      const color = colors[index];
+      const x = index === 0 ? radius : (index + 1) * radius * 1.2;
+      ctx.beginPath();
+      ctx.arc(x, radius, radius, 0, 2 * Math.PI);
+      ctx.fillStyle = color;
+      ctx.fill();
+    }
+    return () => {
+      if (canvasRef.current) {
+        ctx.clearRect(
+          0,
+          0,
+          canvasRef.current!.width,
+          canvasRef.current!.height
+        );
+      }
+    };
+  }, [canvasRef.current, colors]);
+
+  const onCopyImageClick = () => {
+    if (!canvasRef.current) {
+      return;
+    }
+    canvasRef.current.toBlob((blob) => {
+      if (blob) {
+        const item = new ClipboardItem({ "image/png": blob });
+        navigator.clipboard.write([item]);
+      }
+    });
+  };
+
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "row",
-        flexWrap: "wrap",
-      }}
-    >
-      {colors.map((color) => (
-        <div className={styles.selectedColorContainer} key={color}>
-          <div
-            key={color}
-            style={{ backgroundColor: color, color }}
-            className={`${styles.selectedColor} ${styles.colorBox}`}
-          >
-            <CgCloseO
-              className={styles.removeButton}
-              onClick={() => {
-                onRemove(color);
-              }}
-              fill="red"
-            />
-            {(!actions || actions.includes("add-to-pallette")) && (
-              <ImHeart
-                className={styles.favButton}
-                onClick={() => onFavClick(color)}
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+          position: "relative",
+        }}
+      >
+        {colors.map((color) => (
+          <div className={styles.selectedColorContainer} key={color}>
+            <div
+              key={color}
+              style={{ backgroundColor: color, color }}
+              className={`${styles.selectedColor} ${styles.colorBox}`}
+            >
+              <CgCloseO
+                className={styles.removeButton}
+                onClick={() => {
+                  onRemove(color);
+                }}
+                fill="red"
               />
-            )}
-            {(!actions || actions.includes("copy-to-clipboard")) && (
-              <FaCopy
-                className={styles.favButton}
-                onClick={() => onCopyClick(color)}
-              />
-            )}
+              {(!actions || actions.includes("add-to-pallette")) && (
+                <ImHeart
+                  className={styles.favButton}
+                  onClick={() => onFavClick(color)}
+                />
+              )}
+              {(!actions || actions.includes("copy-to-clipboard")) && (
+                <FaCopy
+                  className={styles.favButton}
+                  onClick={() => onCopyClick(color)}
+                />
+              )}
+            </div>
           </div>
+        ))}
+        <div
+          style={{
+            width: "100vw",
+            height: "100px",
+            position: "fixed",
+            top: 0,
+            left: 0,
+            visibility: "hidden",
+          }}
+        >
+          <canvas
+            ref={canvasRef}
+            width={"100%"}
+            style={{
+              height: "100%",
+            }}
+          ></canvas>
         </div>
-      ))}
+      </div>
+      <button type="button" onClick={onCopyImageClick}>
+        Copy Image
+      </button>
     </div>
   );
 };
