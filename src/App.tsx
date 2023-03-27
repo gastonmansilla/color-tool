ff879af6995e03fe365a57e9fc4cc7976dd4b241
Gaston Mansilla <gastonmansilla@MBP-de-Gaston.fibertel.com.ar>
2023-03-27 16:28:27 -0300

    feat(copy-image): forward component ref
    forward color box component ref in order to expose the copy image method for it to be called from outside the component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import "./App.css";
 import { Tones } from "./components/Tones";
-import { usePaletteColors } from "./stores/paletteColors";
-import { ImHeart } from "react-icons/im";
-import { ColorBox } from "./components/ColorBox";
 import { Palette } from "./components/Palette";
+
+const hueRange: [number, number] = [0, 360];
 
 function App() {
   return (
@@ -12,42 +11,63 @@ function App() {
         <h1>Color tool</h1>
         <section className="jewel">
           <h2>Jewel tones</h2>
-          <Tones saturationRange={[73, 83]} brightnessRange={[56, 76]} />
+          <Tones
+            hueRange={hueRange}
+            saturationRange={[73, 83]}
+            brightnessRange={[56, 76]}
+          />
         </section>
         <section>
           <h2>Pastel tones</h2>
-          <Tones saturationRange={[14, 21]} brightnessRange={[89, 96]} />
+          <Tones
+            hueRange={hueRange}
+            saturationRange={[14, 21]}
+            brightnessRange={[89, 96]}
+          />
         </section>
         <section>
           <h2>Earth tones</h2>
-          <Tones saturationRange={[36, 41]} brightnessRange={[36, 77]} />
+          <Tones
+            hueRange={hueRange}
+            saturationRange={[36, 41]}
+            brightnessRange={[36, 77]}
+          />
         </section>
         <section>
           <h2>Neutral tones</h2>
-          <Tones saturationRange={[1, 10]} brightnessRange={[99, 70]} />
+          <Tones
+            hueRange={hueRange}
+            saturationRange={[1, 10]}
+            brightnessRange={[70, 99]}
+          />
         </section>
         <section>
           <h2>Fluorescent tones</h2>
-          <Tones saturationRange={[100, 63]} brightnessRange={[100, 82]} />
+          <Tones
+            hueRange={hueRange}
+            saturationRange={[63, 100]}
+            brightnessRange={[82, 100]}
+          />
         </section>
         <section>
           <h2>Shades</h2>
-          <Tones saturationRange={[0, 0]} brightnessRange={[0, 100]} />
+          <Tones
+            hueRange={hueRange}
+            saturationRange={[0, 0]}
+            brightnessRange={[0, 100]}
+          />
+        </section>
+        <section>
+          <h2>All colors</h2>
+          <Tones
+            hueRange={hueRange}
+            saturationRange={[100, 0]}
+            brightnessRange={[100, 0]}
+          />
         </section>
       </main>
       <div className="palette">
-        <div className="content">
-          <section>
-            <h1>Palette</h1>
-            <p>
-              Select colors from any picker and add them to the palette clicking
-              on the <ImHeart />
-            </p>
-          </section>
-          <div>
-            <Palette />
-          </div>
-        </div>
+        <Palette />
       </div>
     </>
   );
--- src/components/ColorBox.tsx
+++ src/components/ColorBox.tsx
@@ -3,138 +3,146 @@ import { CgCloseO } from "react-icons/cg";
 import { ImHeart } from "react-icons/im";
 import { IoCopy as FaCopy } from "react-icons/io5";
 import { usePaletteColors } from "../stores/paletteColors";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, forwardRef, useImperativeHandle } from "react";
 export type ColorBoxProps = {
   actions?: ("add-to-palette" | "copy-to-clipboard")[];
   colors: string[];
   onRemove: (color: string) => void;
   onCopy: (color: string) => void;
 };
+export type ColorBoxRef = {
+  copyImage: () => void;
+};
 
-export const ColorBox = ({
-  colors,
-  onRemove,
-  actions,
-  onCopy,
-}: ColorBoxProps) => {
-  const [, setPalette] = usePaletteColors();
-  const canvasRef = useRef<HTMLCanvasElement>(null);
+export const ColorBox = forwardRef(
+  (
+    { colors, onRemove, actions, onCopy }: ColorBoxProps,
+    ref: React.ForwardedRef<ColorBoxRef>
+  ) => {
+    const [, setPalette] = usePaletteColors();
+    const canvasRef = useRef<HTMLCanvasElement>(null);
 
-  const onFavClick = (color: string) => {
-    setPalette({ add: color });
-  };
-
-  const onCopyClick = (color: string) => {
-    navigator.clipboard.writeText(color);
-    onCopy(color);
-  };
-
-  useEffect(() => {
-    const ctx = canvasRef.current?.getContext("2d");
-    if (!ctx) {
-      return;
-    }
-
-    const radius = 50;
-    const canvasWidth =
-      colors.length == 1 ? radius * 2 : colors.length * radius * 1.2 + radius;
-
-    canvasRef.current!.width = canvasWidth;
-    canvasRef.current!.height = 100;
-
-    for (let index = 0; index < colors.length; index++) {
-      const color = colors[index];
-      const x = index === 0 ? radius : (index + 1) * radius * 1.2;
-      ctx.beginPath();
-      ctx.arc(x, radius, radius, 0, 2 * Math.PI);
-      ctx.fillStyle = color;
-      ctx.fill();
-    }
-    return () => {
-      if (canvasRef.current) {
-        ctx.clearRect(
-          0,
-          0,
-          canvasRef.current!.width,
-          canvasRef.current!.height
-        );
-      }
+    const onFavClick = (color: string) => {
+      setPalette({ add: color });
     };
-  }, [canvasRef.current, colors]);
 
-  const onCopyImageClick = () => {
-    if (!canvasRef.current) {
-      return;
-    }
-    canvasRef.current.toBlob((blob) => {
-      if (blob) {
-        const item = new ClipboardItem({ "image/png": blob });
-        navigator.clipboard.write([item]);
+    const onCopyClick = (color: string) => {
+      navigator.clipboard.writeText(color);
+      onCopy(color);
+    };
+
+    const copyImage = () => {
+      if (!canvasRef.current) {
+        return;
       }
-    });
-  };
+      canvasRef.current.toBlob((blob) => {
+        if (blob) {
+          const item = new ClipboardItem({ "image/png": blob });
+          navigator.clipboard.write([item]);
+        }
+      });
+    };
 
-  return (
-    <div style={{ display: "flex", flexDirection: "column" }}>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          flexWrap: "wrap",
-          position: "relative",
-        }}
-      >
-        {colors.map((color) => (
-          <div className={styles.selectedColorContainer} key={color}>
-            <div
-              key={color}
-              style={{ backgroundColor: color, color }}
-              className={`${styles.selectedColor} ${styles.colorBox}`}
-            >
-              <CgCloseO
-                className={styles.removeButton}
-                onClick={() => {
-                  onRemove(color);
-                }}
-                fill="red"
-              />
-              {(!actions || actions.includes("add-to-palette")) && (
-                <ImHeart
-                  className={styles.favButton}
-                  onClick={() => onFavClick(color)}
-                />
-              )}
-              {(!actions || actions.includes("copy-to-clipboard")) && (
-                <FaCopy
-                  className={styles.favButton}
-                  onClick={() => onCopyClick(color)}
-                />
-              )}
-            </div>
-          </div>
-        ))}
+    useImperativeHandle(ref, () => {
+      return {
+        copyImage: () => {
+          copyImage();
+        },
+      };
+    });
+
+    useEffect(() => {
+      const ctx = canvasRef.current?.getContext("2d");
+      if (!ctx) {
+        return;
+      }
+
+      const radius = 50;
+      const canvasWidth =
+        colors.length == 1 ? radius * 2 : colors.length * radius * 1.2 + radius;
+
+      canvasRef.current!.width = canvasWidth;
+      canvasRef.current!.height = 100;
+
+      for (let index = 0; index < colors.length; index++) {
+        const color = colors[index];
+        const x = index === 0 ? radius : (index + 1) * radius * 1.2;
+        ctx.beginPath();
+        ctx.arc(x, radius, radius, 0, 2 * Math.PI);
+        ctx.fillStyle = color;
+        ctx.fill();
+      }
+      return () => {
+        if (canvasRef.current) {
+          ctx.clearRect(
+            0,
+            0,
+            canvasRef.current!.width,
+            canvasRef.current!.height
+          );
+        }
+      };
+    }, [canvasRef.current, colors]);
+
+    return (
+      <div style={{ display: "flex", flexDirection: "column" }}>
         <div
           style={{
-            width: "100vw",
-            height: "100px",
-            position: "fixed",
-            top: 0,
-            left: 0,
-            visibility: "hidden",
+            display: "flex",
+            flexDirection: "row",
+            flexWrap: "wrap",
+            position: "relative",
           }}
         >
-          <canvas
-            ref={canvasRef}
-            width={"100%"}
+          {colors.map((color) => (
+            <div className={styles.selectedColorContainer} key={color}>
+              <div
+                key={color}
+                style={{ backgroundColor: color, color }}
+                className={`${styles.selectedColor} ${styles.colorBox}`}
+              >
+                <CgCloseO
+                  className={styles.removeButton}
+                  onClick={() => {
+                    onRemove(color);
+                  }}
+                  fill="red"
+                />
+                {(!actions || actions.includes("add-to-palette")) && (
+                  <ImHeart
+                    className={styles.favButton}
+                    onClick={() => onFavClick(color)}
+                  />
+                )}
+                {(!actions || actions.includes("copy-to-clipboard")) && (
+                  <FaCopy
+                    className={styles.favButton}
+                    onClick={() => onCopyClick(color)}
+                  />
+                )}
+              </div>
+            </div>
+          ))}
+          <div
             style={{
-              height: "100%",
+              width: "100vw",
+              height: "100px",
+              position: "fixed",
+              top: 0,
+              left: 0,
+              visibility: "hidden",
             }}
-          ></canvas>
+          >
+            <canvas
+              ref={canvasRef}
+              width={"100%"}
+              style={{
+                height: "100%",
+              }}
+            ></canvas>
+          </div>
         </div>
       </div>
-      <button type="button" onClick={onCopyImageClick}>
-        Copy Image
-      </button>
-    </div>
-  );
-};
+    );
+  }
+);
